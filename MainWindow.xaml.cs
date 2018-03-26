f54a37cfa5f48e7facea5c6c7ccce3dbb67b4e5e
IanMcT <ian.mctavish@tldsb.on.ca>
2018-03-26 13:45:07 -0400

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ReadListOfTeams
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPickFileRead_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void btnPickFileWrite_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
