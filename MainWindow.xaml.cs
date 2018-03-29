b23e1ddab963c332959193a190b1361d991435c2
IanMcT <ian.mctavish@tldsb.on.ca>
2018-03-29 06:15:10 -0400

    Update MainWindow.xaml.cs
    Added code to read through file.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,6 +20,7 @@ namespace ReadListOfTeams
     /// </summary>
     public partial class MainWindow : Window
     {
+        string readfileName;
         public MainWindow()
         {
             InitializeComponent();
@@ -27,12 +28,56 @@ namespace ReadListOfTeams
 
         private void btnPickFileRead_Click(object sender, RoutedEventArgs e)
         {
-            
+            //add Reference: - System.Windows.Forms
+            System.Windows.Forms.OpenFileDialog openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            System.Windows.Forms.DialogResult dialogResult = openFileDialog.ShowDialog();
+            if (dialogResult == System.Windows.Forms.DialogResult.OK) {
+                readfileName = openFileDialog.FileName;
+                lblFileRead.Content = readfileName;
+                btnPickFileWrite.IsEnabled = true;
+            }
+           
         }
 
         private void btnPickFileWrite_Click(object sender, RoutedEventArgs e)
         {
+            System.Windows.Forms.SaveFileDialog saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            System.Windows.Forms.DialogResult dialogResult = saveFileDialog.ShowDialog();
+            if (dialogResult == System.Windows.Forms.DialogResult.OK)
+            {
+                try {
+                    System.IO.StreamReader streamReader = new System.IO.StreamReader(readfileName);
 
+                    /*
+                     * Loop through file, we are going to pull data for the following:
+                     * "city":
+                        "key":
+                        "postal_code":
+                        "team_number":
+                    */
+                    String teams = "";
+                    while (!streamReader.EndOfStream)
+                    {
+                        
+                        string line = streamReader.ReadLine();
+                        if (line.Contains("\"city\":"))
+                        {
+                            //Get rid of front - needed to add 6 to deal with spaces
+                            line = line.Remove(0, "\"city\":".Length+6);
+                            //Get rid of ", at the end
+                            line = line.Substring(0, line.Length - 3);
+                            //MessageBox.Show(line);//troubleshooting
+                            teams += line + "\n";
+                        }
+                        
+                    }
+                    MessageBox.Show(teams);
+                } catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
         }
     }
 }
+ 
